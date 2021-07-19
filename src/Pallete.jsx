a1dbde41eab9c9e0eaa23563e6aebc398fe7ed07
Rahat47 <djrayhan8@gmail.com>
2021-07-19 23:25:31 +0600

    initial commit with a simple component

---
+++ src/Pallete.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pallete = props => {
+    console.log(props);
+    return (
+        <div className="Palette">
+            {/* Navbar Goes Here */}
+            <div className="Palette-colors">{/* Bunch of color boxes */}</div>
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Pallete;
