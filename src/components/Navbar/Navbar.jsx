06d9e48ede457b585050626b955888d61f82e860
Rahat47 <djrayhan8@gmail.com>
2021-07-22 11:26:35 +0600

    added material ui & made select option in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
+import { MenuItem, Select } from "@material-ui/core";
+import Slider from "rc-slider";
+import { useState } from "react";
 
-const Navbar = ({ level, setLevel }) => {
+const Navbar = ({ level, setLevel, handleChange }) => {
+    const [format, setFormat] = useState("hex");
+    const handleChangeFormat = event => {
+        setFormat(event.target.value);
+        handleChange(event.target.value);
+    };
+
     return (
         <header className="Navbar">
             <div className="logo">
@@ -19,6 +27,16 @@ const Navbar = ({ level, setLevel }) => {
                         onAfterChange={level => setLevel(level)}
                     />
                 </div>
+            </div>
+
+            <div className="select-container">
+                <Select defaultValue={format} onChange={handleChangeFormat}>
+                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                    <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                    <MenuItem value="rgba">
+                        RGBA - rgb(255,255,255,1.0)
+                    </MenuItem>
+                </Select>
             </div>
         </header>
     );
--- src/components/Pallete/Pallete.jsx
+++ src/components/Pallete/Pallete.jsx
@@ -5,17 +5,26 @@ import Navbar from "../Navbar/Navbar.jsx";
 
 const Pallete = ({ pallete }) => {
     const [level, setLevel] = useState(500);
+    const [format, setFormat] = useState("hex");
+
+    const changeFormat = val => {
+        setFormat(val);
+    };
 
     return (
         <div className="Pallete">
             {/* Navbar Goes Here */}
-            <Navbar level={level} setLevel={setLevel} />
+            <Navbar
+                level={level}
+                setLevel={setLevel}
+                handleChange={changeFormat}
+            />
 
             <div className="Pallete-colors">
                 {/* Bunch of color boxes */}
                 {pallete.colors[level].map((color, i) => (
                     <ColorBox
-                        background={color.hex}
+                        background={color[format]}
                         name={color.name}
                         key={i}
                     />
